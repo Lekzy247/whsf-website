82371c5211708d9fc3d456741958521f412e403b
Lekzy247 <info@worldhsfoundation.org>
2026-08-04 21:01:46 -0700

    Update programmes hero image

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const WHSF_CACHE_NAME = 'whsf-pwa-v163';
+﻿const WHSF_CACHE_NAME = 'whsf-pwa-v164';
 const WHSF_OFFLINE_URL = '/offline.html';
 
 const WHSF_CORE_ASSETS = [
@@ -65,6 +65,7 @@ const WHSF_CORE_ASSETS = [
   '/assets/impact/whsf-girls-certificates-2026.jpg',
   '/assets/partners/whsf-partnership-recognition.jpg',
   '/assets/contact/whsf-contact-learner.jpg',
+  '/assets/programs/whsf-drone-practical-header.jpg',
   '/assets/awards/london-data-center-world-award-2025-winner.jpg',
   '/assets/about/about-us-board.webp',
   '/assets/programs/ai-career-connect-social.png',
